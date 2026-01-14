ace165fac7a39c115784a097b1b8cc4d21780667
Ira Tretiakova <iratretiakova@MacBook-Pro-Ira.local>
2026-01-14 13:22:54 +0200

    initial commit

---
+++ NotificationCenter/NotificationCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationCenterApp.swift
+//  NotificationCenter
+//
+//  Created by Ira Tretiakova on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct NotificationCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
